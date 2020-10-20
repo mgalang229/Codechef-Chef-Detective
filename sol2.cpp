f8c725318066d0af6aea17797eeba70dc4410b4e
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-20 20:13:27 +0800

    Update sol2.cpp

--- sol2.cpp
+++ sol2.cpp
@@ -1,24 +1,26 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-
 int main() {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
 	int n;
 	cin >> n;
 	int a[n];
 	set<int> s;
-	for(int i=1; i<=n; ++i) {
+	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
-		s.insert(i); 
+		s.insert(i);
 	}
-	for(int i=1; i<=n; ++i)
-		if(s.find(a[i])!=s.end())
+	for (int i = 1; i <= n; i++) {
+		if (s.find(a[i]) != s.end()) {
 			s.erase(a[i]);
-	for(set<int>::iterator it=s.begin(); it!=s.end(); ++it)
+		}
+	}
+	for (set<int>::iterator it = s.begin(); it != s.end(); it++) {
 		cout << *it << " ";
-	cout << "\n";
+	}
+	cout << '\n';
+	return 0;
 }
