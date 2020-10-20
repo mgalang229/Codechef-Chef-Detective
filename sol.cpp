2dd1b9dde8b1e42ec4615137f4fc845f1ce6228f
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-20 19:17:48 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,25 +1,24 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
+#define ll long long
+
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
 	int n;
 	cin >> n;
-	vector<bool> v(n, false);
-	for (int i = 0; i < n; i++) {
+	bool vis[n+1];
+	memset(vis, 0, sizeof(vis));
+	for(int i=0; i<n; ++i) {
 		int tmp;
 		cin >> tmp;
-		if (tmp != 0) {
-			v[tmp - 1] = true;
-		}
+		if(tmp!=0)
+			vis[tmp-1]=1;
 	}
-	for (int i = 0; i < n; i++) {
-		if (v[i] == false) {
-			cout << i + 1 << " ";
-		}
-	}
-	cout << '\n';
-	return 0;
+	for(int i=0; i<n; ++i)
+		if(!vis[i])
+			cout << i+1 << " ";
+	cout << "\n";
 }
